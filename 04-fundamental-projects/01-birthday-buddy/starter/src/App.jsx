9ffa9d3502132b8549b4e19ffdd129f83b271b39
Abhay Dutt <dutt.abhay@gmail.com>
2023-11-04 13:51:51 +0530

    completed first project

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+import data from "./data";
+import "./style.css";
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <div className="cont">
+      {people.map((person) => {
+        const { image, name, age, id } = person;
+        return (
+          <div key={id}>
+            <div
+              style={{
+                backgroundImage: `url(${image})`,
+                width: "40px",
+                height: "40px",
+                backgroundSize: "cover",
+                borderRadius: "50px",
+              }}
+            ></div>
+            <h3>{`${people.length} birthdays today`}</h3>
+            <h3>{name}</h3>
+            <h5>{age}</h5>
+          </div>
+        );
+      })}
+      <button className = 'btn' onClick={()=>setPeople([])}>clear all</button>
+    </div>
+  );
 };
 export default App;
