9ec874e22ea262f71a72c3bbcdf0bc8a5651572c
Abhay Dutt <dutt.abhay@gmail.com>
2023-11-19 23:14:14 +0530

    start axios

--- 05-axios-tutorial/src/components/Title.js
+++ 05-axios-tutorial/src/components/Title.js
@@ -1,9 +1,12 @@
+// import FirstRequest from '../examples/1-first-request';
+import Headers from "../examples/2-headers";
 const Title = () => {
   return (
     <div className='title'>
       <h1>
         <span>axios</span> tutorial
       </h1>
+      <Headers></Headers>
     </div>
   );
 };
