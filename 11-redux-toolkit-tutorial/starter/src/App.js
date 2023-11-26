3946b936bf641bfe694914c7dae3e7bb5b88c482
Abhay Dutt <dutt.abhay@gmail.com>
2023-11-26 16:24:12 +0530

    learning redux-toolkit

--- 11-redux-toolkit-tutorial/starter/src/App.js
+++ 11-redux-toolkit-tutorial/starter/src/App.js
@@ -1,4 +1,24 @@
+import {useEffect} from 'react';
+import NavBar from "./components/NavBar";
+import CartContainer from "./components/CartContainer";
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
+
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { isOpen } = useSelector((state)=>state.modal);
+  const {cartItems} = useSelector((state)=>state.cart);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(calculateTotals());
+  },[cartItems]);
+  return (
+    <main>
+    {isOpen && <Modal></Modal>}
+    <NavBar></NavBar>
+    <CartContainer></CartContainer>
+    </main>
+  );
 }
 export default App;
